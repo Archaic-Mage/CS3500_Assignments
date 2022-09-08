ee91ec0aadfe3bd5938d01fe407087a8a8ab4e86
Archaic-Mage <sohasoham811@gmail.com>
2022-09-08 13:00:30 +0530

    complete Q1 a03

--- assignment3/Q1/client.c
+++ assignment3/Q1/client.c
@@ -4,49 +4,80 @@
     Topic: Client Server communication using message queues
 */
 
-#include <sys/ipc.h>
-#include <sys/msg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define MAX 1000000
+#define MAX 1000
 
+// structure for message queue
 typedef struct mesg_buffer {
-    long mesg_type;
-    char mesg_text[1000000];
+	long mesg_type;
+	char mesg_text[1000];
 } message;
-  
+
+
 int main()
 {
-    key_t c2s_key, s2c_key;
-    int c2s;
-    int s2c;
+    //keys for c2s and s2c process respectively
+	key_t key, key2;
+
+    //identifiers for the process
+	int c2s_id;
+    int s2c_id;
+
+    //message queue elements
     message c2s_msg;
-    message s2c_msg;
 
-    c2s_key = 12345678;
-    s2c_key = 87654321;
-  
-    // msgget creates a message queue
-    // and returns identifier
-    c2s = msgget(c2s_key, 0666 | IPC_CREAT);
-    s2c = msgget(s2c_key, 0666 | IPC_CREAT);
-    c2s_msg.mesg_type = 0;
+    //setting the key values
+	key = 12345678;
+    key2 = 87654321;
 
-    printf("Enter System Call: \n");
-    fgets(c2s_msg.mesg_text,MAX,stdin);
+    printf("|******* Client Side *******|\n");
 
-    while(strcmp(c2s_msg.mesg_text, "End") != 0) {
+    while(true) {
+
+        printf("\n");
+
+        // msgget creates a message queue
+	    // and returns identifier
+        c2s_id = msgget(key, 0666 | IPC_CREAT);
+	    c2s_msg.mesg_type = 1;
+
+        printf("...Waiting For User Input....\n");
+        printf("Enter System Call: ");
+        fgets(c2s_msg.mesg_text, MAX, stdin);
+
         // msgsnd to send message
-        msgsnd(c2s, &c2s_msg, sizeof(c2s_msg), 0);
+	    if(msgsnd(c2s_id, &c2s_msg, sizeof(c2s_msg), 0) == -1) {
+            printf("Failed to Send the Data!\n");
+            printf("...Please! Try Again....\n");
+        } else {
+            printf("...Data Sent Successfully...\n");
+        }
 
-        printf("send data\n");
+        //exit condition
+        if(strcmp(c2s_msg.mesg_text, "End\n") == 0) break;
 
-        printf("Enter System Call: \n");
-        fgets(c2s_msg.mesg_text,MAX,stdin);
+        //Output Message from Server
+        message s2c_msg;
+
+        s2c_id = msgget(key2, 0666 | IPC_CREAT);
+
+        printf("...Client waiting for output!..\n");
+
+        if(msgrcv(s2c_id, &s2c_msg, sizeof(s2c_msg), 1, 0) == -1) {
+            printf("Failed to Recieve the Data!\n");
+            printf("....Please! Try Again.....\n");
+        } else {
+            printf("Data recieved is : %s \n", s2c_msg.mesg_text);
+        }        
     }
-  
-    return 0;
+
+	return 0;
 }
--- assignment3/Q1/server.c
+++ assignment3/Q1/server.c
@@ -4,41 +4,133 @@
     Topic: Client Server communication using message queues
 */
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h> 
+#include <errno.h> 
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <sys/stat.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define MAX 1000000
-
+// structure for message queue
 typedef struct mesg_buffer {
-    long mesg_type;
-    char mesg_text[1000000];
+	long mesg_type;
+	char mesg_text[1000];
 } message;
-  
+
+
+//Stores the file descriptor
+int fd = -1;
+
+//flag for valid command
+int flag = 0;
+
+//function to process the system calls
+int processCall(char* text) {
+
+    //open(2) system call
+    if(strcmp(text, "open\n") == 0) {
+        flag = 1;
+        fd = open("./temp.txt", O_RDWR | O_CREAT, S_IRWXU);
+        return fd;
+    } 
+
+    //close(1) system call
+    if(strcmp(text, "close\n") == 0) {
+        flag = 1;
+        int out = close(fd);
+        fd = -1;
+        return out;
+    }
+
+    //read(3) system call
+    if(strcmp(text, "read\n") == 0) {
+        flag = 1;
+        char* buff;
+        buff = (char *) malloc(sizeof(char)*1000);
+        size_t out = read(fd, buff, 1000);
+
+        return (int) out;
+    }
+
+    //write(3) system call
+    if(strcmp(text, "write\n") == 0) {
+        flag = 1;
+        if(fd == -1) return -1;
+        char buff[] = "This is Writing\n";
+        int out = (int) write(fd, buff, sizeof(buff));
+        return out;
+    }
+
+    //fchmod(2) system call
+    if(strcmp(text, "fchmod\n") == 0) {
+        flag = 1;
+        return fchmod(fd, S_IRWXU | S_IRWXG | S_IRWXO);
+    }
+
+    flag = 0;
+    return -1;
+    
+}
+
 int main()
 {
-    key_t c2s_key, s2c_key;
-    int c2s;
-    int s2c;
-    message c2s_msg;
+    //keys for c2s and s2c process respectively
+	key_t key, key2;
+
+    //indentifiers for the process
+	int c2s_id;
+    int s2c_id;
+
+    //message queue elements
     message s2c_msg;
 
-    c2s_key = 12345678;
-    s2c_key = 87654321;
-  
-    // msgget creates a message queue
-    // and returns identifier
-    c2s = msgget(c2s_key, 0666 | IPC_CREAT);
-    s2c = msgget(s2c_key, 0666 | IPC_CREAT);
-    
-    msgrcv(c2s, &c2s_msg, sizeof(c2s_msg), 0, 0);
+    //setting the key values
+	key = 12345678;
+    key2 = 87654321;
 
-    while(strcmp(c2s_msg.mesg_text, "End") != 0) {
-        printf("%s", c2s_msg.mesg_text);
+    printf("|******* Server Side *******|\n");
+
+    while(true) {
+
+        printf("\n");
+        printf("..Server Waiting for User System Call..\n");
+
+        //Recieves System call from client side
+        message c2s_msg;
+
+        // msgget creates a message queue
+	    // and returns identifier
+        c2s_id = msgget(key, 0666 | IPC_CREAT);
+
+        // recieves the messages from the message queue
+        msgrcv(c2s_id, &c2s_msg, sizeof(c2s_msg), 1, 0);
+
+        //exit condition
+        if(strcmp(c2s_msg.mesg_text, "End\n") == 0) break;
+
+        printf("...Server is working!...\n");
+
+        s2c_id = msgget(key2, 0666 | IPC_CREAT);
+        s2c_msg.mesg_type = 1;
+
+        int result = processCall(c2s_msg.mesg_text);
+        if(flag) sprintf(s2c_msg.mesg_text, "%d", result);
+        else sprintf(s2c_msg.mesg_text, "Enter Valid System Call");
+
+        //sends the data returned by execution of such commands
+        msgsnd(s2c_id, &s2c_msg, sizeof(s2c_msg), 0);
+
+        printf("--- Server done working! ----\n");
     }
-  
-    return 0;
+
+    // to destroy the message queue
+	msgctl(c2s_id, IPC_RMID, NULL);
+
+    remove("./temp.txt");
+
+	return 0;
 }
